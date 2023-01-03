0f7825263b31dd4ce8ded443be0add552496e7ce
DJT777 <djt.2012.777@gmail.com>
2023-01-03 02:22:00 -0600

    Full backup

---
+++ WebDevBirdStats/main.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, url_for, render_template, request
+import pymysql
+import json
+
+
+
+app = Flask(__name__)
+
+
+
+def mysqlconnect():
+    conn = pymysql.connect(
+        host='localhost',
+        user='dylan',
+        password='cookies',
+        db="nano_detections",
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor)
+    cursor = conn.cursor()
+    cursor.execute("Select SPECIES, COUNT(*) as COUNT FROM detections GROUP BY species")
+    output = cursor.fetchall()
+
+    conn.close()
+    return output
+
+@app.route("/", methods=['GET'])
+def home():
+    return render_template("base.html", json_file=json_file)
+
+@app.route("/pie", methods=['GET'])
+def pie():
+    return render_template("pie.html", json_file=json_file)
+
+@app.route("/bar", methods=['GET'])
+def bar():
+    return render_template("bar.html", json_file=json_file)
+
+if __name__ == "__main__":
+    output = mysqlconnect()
+    print(f"json: {json.dumps(output)}")
+    with open('static/json_data.json', 'w') as outfile:
+        outfile.write(json.dumps(output, indent=4))
+    outfile.close()
+    json_file = json.dumps(output, indent=4)
+    app.run(debug=True)
---
+++ classify-sql-composite/main.py
@@ -0,0 +1,65 @@
+
+import pymysql
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import cv2
+import os
+
+model = tf.keras.models.load_model('/home/dylan/Desktop/data_science_work/working-dir/saved-models/Composite-VGG16-model_fine_tuned')
+try:
+        connection = pymysql.connect(host='127.0.0.1',
+                                             database='nano_detections',
+                                             user='dylan',
+                                             password='cookies',
+                                             )
+        #get class list
+        class_array = os.listdir('/home/dylan/Desktop/data_science_work/datasets/Composite Classifier')
+        class_array.sort()
+        print(class_array)
+        print("LENGTH OF ARRAY: " + str(len(class_array)))
+        cursor = connection.cursor()
+        #cursor2 = connection.cursor()
+        sql_select_query = """select * from detections where IS_CLASSIFIED = 0"""
+
+        # set variable in query
+        cursor.execute(sql_select_query)
+        # fetch result
+        record = cursor.fetchall()
+        print(len(record))
+        for row in record:
+            id = row[0]
+            date = row[1]
+            time = row[2]
+            conf = row[3]
+            track_id = row[4]
+            img_blob = row[5]
+            nparr = np.fromstring(img_blob, np.uint8)
+            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            resized_image = cv2.resize(img, (224 , 224))
+            resized_image = resized_image.reshape(1,224,224,3)
+            predict_x = model.predict(resized_image)
+            #print("Length of prediction array: " + len(predict_x))
+            classes_x = np.argmax(predict_x)
+            #class_index = classes_x[0]
+            #print(class_index)
+            predicted_class = class_array[classes_x]
+            #predictions = model.predict(resized_image)
+            print("CLASS INDEX:" + str(classes_x))
+            print("CLASS PREDICTED: " + predicted_class)
+            print(predicted_class)
+            #cv2.imshow('image', img)
+            #cv2.imshow('resized-image', resized_image)
+            #cv2.waitKey()
+            sql_update_query = "UPDATE detections SET IS_CLASSIFIED = TRUE, SPECIES = %s WHERE PRIMARY_KEY = %s "
+            cursor.execute(sql_update_query,  (predicted_class, id))
+            connection.commit()
+
+except Exception as e:
+    print("Error reading data from MySQL table", e)
+finally:
+   if connection.open:
+    connection.close()
+       ##cursor2.close()
+    cursor.close()
+    print("MySQL connection is closed")
